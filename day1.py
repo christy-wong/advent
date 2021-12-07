9989e91a26bfbf1ab54f9b682ee22343fb9be1d7
Christy Wong <christyw@users.noreply.github.com>
2021-12-06 16:32:16 -0800

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,40 +1,5 @@
-f = open('moves.txt')
-# lines = [i for i in f.readlines()]
-lines = [line.rstrip('\n') for line in f]
-
-hor = 0
-dep = 0
-i = 0
-while i < len(lines):
-      word = lines[i][0:lines[i].find(' ')]
-      number = int(lines[i][lines[i].find(' ')+1:len(lines[i])])
-      if word == 'forward':
-        hor += number
-        i += 1
-      elif word == 'down':
-        dep += number
-        i += 1
-      else:
-        dep -= number
-        i += 1
-
-hor = 0
-dep = 0
-aim = 0
-i = 0
-while i < len(lines):
-      word = lines[i][0:lines[i].find(' ')]
-      number = int(lines[i][lines[i].find(' ')+1:len(lines[i])])
-      if word == 'down':
-        i += 1
-        aim += number
-      elif word == 'up':
-        i += 1
-        aim -= number
-      else:
-        hor += number
-        i += 1
-        dep += number * aim
+f = open('depths.txt')
+lines = [int(i) for i in f.readlines()]
 
 def count_increases(count, increase, lines):
 
@@ -47,8 +12,7 @@ def count_increases(count, increase, lines):
 
   return increase
 
-f = open('depths.txt')
-lines = [int(i) for i in f.readlines()]
+
 i = 0
 increase = 0
 while i < len(lines) - 3:
