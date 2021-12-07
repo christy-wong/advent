9787c8d56947d118878bb0234f73603b9c705a5a
Christy Wong <cwong@flexport.com>
2021-12-06 22:47:43 -0800

    not there yet

--- day3/day3.py
+++ day3/day3.py
@@ -48,6 +48,63 @@ def episilon(lines):
   # return string
   return int(string, 2)
 
+
+
+def oxygen_rating(lines):
+  i = 0
+  pos = 0
+  count_ones = 0
+  count_zeros = 0
+  while len(lines) > 1:
+    while i < len(lines):
+      if lines[i][pos] == '0':
+        count_zeros += 1
+      else:
+        count_ones += 1
+      i += 1
+    for item in lines[:]:
+      if len(lines) == 1:
+        break
+      if count_ones >= count_zeros and item[pos] == '0':
+        lines.remove(item)
+      if count_zeros > count_ones and item[pos] == '1':
+        lines.remove(item)
+    if len(lines) == 1:
+      break
+    pos += 1
+    i = 0
+    count_ones = 0
+    count_zeros = 0
+  return int(lines[0], 2)
+
+def co2_rating(lines):
+  i = 0
+  pos = 0
+  count_ones = 0
+  count_zeros = 0
+  while len(lines) > 1:
+    while i < len(lines):
+      if lines[i][pos] == '0':
+        count_zeros += 1
+      else:
+        count_ones += 1
+      i += 1
+    for item in lines[:]:
+      if len(lines) == 1:
+        break
+      if count_ones >= count_zeros and item[pos] == '1':
+        lines.remove(item)
+      if count_zeros > count_ones and item[pos] == '0':
+        lines.remove(item)
+    if len(lines) == 1:
+      break
+    pos += 1
+    i = 0
+    count_ones = 0
+    count_zeros = 0
+  return int(lines[0], 2)
+
+
 if __name__ == "__main__":
   f = open('rays.txt')
   lines = [line.rstrip('\n') for line in f]
@@ -55,4 +112,6 @@ if __name__ == "__main__":
   print(gamma(lines))
   print(episilon(lines))
   print(gamma(lines)*episilon(lines))
-
+  print(oxygen_rating(lines))
+  print(co2_rating(lines))
+  print(oxygen_rating(lines)*co2_rating(lines))
