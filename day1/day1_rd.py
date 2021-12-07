32b9a57d0f6b1029bb422f430002ab2b2bf84dbe
Ravi Dayabhai <ravi.dayabhai@chime.com>
2021-12-06 21:38:38 -0500

    Finish RD's Part 2 of Day 1

--- day1/day1_rd.py
+++ day1/day1_rd.py
@@ -1,14 +1,21 @@
 import numpy as np
 
 
-def main():
+def increasing_depths(
+    raw_data_file: str = "./depths_rd.txt", window_size: int = 1
+) -> int:
     """
-    Calculates the number of times the provided depth measurements increases
+    Calculates the number of times the provided depth measurements increases;
+    `window_size` provides length of window for windowed sum of elements in
+    `raw_data_file`
     """
-    data = np.loadtxt('./depths_rd.txt')
-    positive_diffs = np.sum(np.diff(data) > 0, axis=0)
-    return positive_diffs
+    data = np.loadtxt(raw_data_file)
+    positive_diffs = np.diff(
+        np.convolve(data, np.ones(window_size, dtype=int), "valid")
+    )
+    return np.sum(positive_diffs > 0, axis=0)
 
 
 if __name__ == "__main__":
-    print(main())  # Solution: 1581
+    print(increasing_depths())  # Part 1 Solution: 1581
+    print(increasing_depths(window_size=3))  # Part 2 Solution:
