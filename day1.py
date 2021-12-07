f9a5f11b9431f81bc40f589cfb61ede9c996e501
Christy Wong <christyw@users.noreply.github.com>
2021-12-06 16:30:45 -0800

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,62 @@
+f = open('moves.txt')
+# lines = [i for i in f.readlines()]
+lines = [line.rstrip('\n') for line in f]
+
+hor = 0
+dep = 0
+i = 0
+while i < len(lines):
+      word = lines[i][0:lines[i].find(' ')]
+      number = int(lines[i][lines[i].find(' ')+1:len(lines[i])])
+      if word == 'forward':
+        hor += number
+        i += 1
+      elif word == 'down':
+        dep += number
+        i += 1
+      else:
+        dep -= number
+        i += 1
+
+hor = 0
+dep = 0
+aim = 0
+i = 0
+while i < len(lines):
+      word = lines[i][0:lines[i].find(' ')]
+      number = int(lines[i][lines[i].find(' ')+1:len(lines[i])])
+      if word == 'down':
+        i += 1
+        aim += number
+      elif word == 'up':
+        i += 1
+        aim -= number
+      else:
+        hor += number
+        i += 1
+        dep += number * aim
+
+def count_increases(count, increase, lines):
+
+  while count < len(lines) - 1:
+    if lines[count + 1] > lines[count]:
+      increase += 1
+      count += 1
+    else:
+      count += 1
+
+  return increase
+
+f = open('depths.txt')
+lines = [int(i) for i in f.readlines()]
+i = 0
+increase = 0
+while i < len(lines) - 3:
+    sum1 = lines[i] + lines[i+1] + lines[i+2]
+    sum2 = lines[i+1] + lines[i+2] + lines[i+3]
+
+  if sum2 > sum1:
+    increase += 1
+    i += 1
+  else:
+    i += 1
