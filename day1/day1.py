f95ca6e85dd5c3bea71924e16b16bccb3f88fcd7
Christy Wong <christyw@users.noreply.github.com>
2021-12-06 17:08:30 -0800

    Update day1.py

--- day1/day1.py
+++ day1/day1.py
@@ -24,9 +24,6 @@ def count_moving_increases(lines):
   return increase
 
 if __name__ == "__main__":
-    # dir = os.path.abspath(os.path.dirname(__file__))
-    # with open(os.path.join(dir, "input.txt")) as file:
-    #     _input = file.read().splitlines()
     f = open('depths.txt')
     lines = [int(i) for i in f.readlines()]
 
