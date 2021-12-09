6892b148dd6e323ddec1eb9326efe0e292df350f
Ravi Dayabhai <ravi.dayabhai@chime.com>
2021-12-08 20:15:32 -0500

    Finish RD's Part 2 of Day 4

--- day4/day4_rd.py
+++ day4/day4_rd.py
@@ -1,5 +1,4 @@
 from collections import Counter
-from itertools import compress
 from typing import List, Tuple
 
 import numpy as np
@@ -13,49 +12,54 @@ def read_data(raw_data_file: str) -> Tuple[List[int], List[List[List[int]]]]:
         called_numbers = list(map(int, f.readline().rstrip().split(",")))
         next(f)
         raw_grids = f.read().split("\n\n")
-
     boards = []
     for raw_grid in raw_grids:
         rows = raw_grid.split("\n")
         boards.append(np.array([list(map(int, row.split())) for row in rows]))
-
     return called_numbers, boards
 
 
 class Game:
+    """
+    Main class governing the playing of the bingo game across multiple `boards`
+    as numbers are called sequentially from `called_numbers`
+    """
+
     def __init__(self, called_numbers: List[int], boards: List[List[List[int]]]):
         self.called_numbers = called_numbers
         self.boards = [BingoBoard(board) for board in boards]
 
-    def you_win_first_score(self) -> int:
-        winner = False
-        while not winner:
-            for called_number in called_numbers:
-                win_statuses = []
-                for board in self.boards:
-                    board.play_round(called_number)
-                    win_statuses.append(board.get_win_status())
-                if any(win_statuses):
-                    break
-            winner = any(win_statuses)
-        winning_board = list(compress(self.boards, win_statuses))[0]
-        return winning_board.get_sum_of_unmatched_numbers() * called_number
-
-    # TODO(rdayabhai)
-    def squid_wins_first_score(self) -> int:
-        pass
-        # for i, called_number in enumerate(called_numbers):
-        #     for board in self.boards:
-        #         board.play_round(called_number)
+    def get_board_scores(self) -> int:
+        """
+        Returns scores for all boards over the entirety of the game; scores are
+        logged as each board wins (i.e., order of winning boards is preserved)
+        """
+        remaining_boards = self.boards.copy()
+        winning_boards = []
+        for called_number in called_numbers:
+            for board in remaining_boards:
+                board.play_round(called_number)
+                if board.get_win_status():
+                    winning_boards.append((board, called_number))
+            remaining_boards = [
+                board
+                for board in remaining_boards
+                if board not in [board for board, _ in winning_boards]
+            ]
+        return [
+            winning_board.get_sum_of_unmatched_numbers() * winning_called_number
+            for winning_board, winning_called_number in winning_boards
+        ]
 
 
 class BingoBoard:
     """
     Class modeling behavior of a single Bingo board
     """
+
     def __init__(self, board_array: np.array):
         self.board = board_array
-        self.matched_numbers_indices = []
+        self.matched_numbers_indices = set()
 
     def _get_indices_of_called_number(
         self, called_number: int
@@ -74,7 +78,7 @@ class BingoBoard:
         matched_indices = self._get_indices_of_called_number(called_number)
         if matched_indices:
             for idx in matched_indices:
-                self.matched_numbers_indices.append(idx)
+                self.matched_numbers_indices.add(idx)
 
     def get_win_status(self) -> bool:
         """
@@ -96,7 +100,7 @@ class BingoBoard:
         Returns the sum of the numbers on the board that have not been called
         """
         sum_array = np.copy(self.board)
-        matched_numbers_indices_array = np.array(self.matched_numbers_indices)
+        matched_numbers_indices_array = np.array(list(self.matched_numbers_indices))
         sum_array[
             matched_numbers_indices_array[:, 0], matched_numbers_indices_array[:, 1]
         ] = 0
@@ -106,5 +110,6 @@ class BingoBoard:
 if __name__ == "__main__":
     called_numbers, boards = read_data("./bingo_rd.txt")
     game = Game(called_numbers, boards)
-    print(game.you_win_first_score())  # Part 1 Solution: 11536
-    print(game.squid_wins_first_score())  # Part 2 Solution:
+    scores = game.get_board_scores()
+    print(scores[0])  # Part 1 Solution: 11536
+    print(scores[-1])  # Part 2 Solution: 1284
