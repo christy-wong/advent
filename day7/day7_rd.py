73a2b4af86fb78336581136fc74629ca72adaa5f
Ravi Dayabhai <ravi.dayabhai@chime.com>
2021-12-09 21:44:25 -0500

    Finish RD's Part 2 of Day 7

--- day7/day7_rd.py
+++ day7/day7_rd.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.optimize import minimize_scalar
 
 
 def read_data(raw_data_file: str) -> np.array:
@@ -10,15 +11,50 @@ def read_data(raw_data_file: str) -> np.array:
     return data
 
 
-def fuel_consumption(data: np.array) -> int:
+def linear_scale_cost_function(data: np.array, m: np.array) -> int:
+    """
+    Cost function for when the fuel costs incrementally scale linearly with steps taken
+    """
+    d = np.abs(data.astype(int) - m.astype(int))
+    return ((d ** 2 + d) / 2).sum().astype(int)
+
+
+def fuel_consumption(data: np.array, cost_scale="constant") -> int:
     """
     Calculates the minimum amount of fuel expended by crab submarines to get
     horizontal alignment based on initial horizontal positions provided by `data`
     """
-    x_med = np.median(data)
-    return int(np.abs(data - x_med).sum())
+    d = np.abs(data - np.median(data))
+    l1_cost_min = int(d.sum())
+
+    # Choose 25% of total integer points around the mean
+    m_star = np.mean(data)
+    search_interval = data.max() - data.min() / 8
+    m_low, m_high = m_star - search_interval, m_star + search_interval
+
+    align_pos = minimize_scalar(
+        linear_scale_cost_function, (m_low, m_high), args=(data,)
+    )
+    m = int(align_pos["x"])
+
+    if cost_scale == "constant":
+        return l1_cost_min
+    elif cost_scale == "linear":
+        return int(linear_scale_cost_function(data, np.ones_like(data) * m))
 
 
 if __name__ == "__main__":
     data = read_data("./crabs_rd.txt")
+    test_data = np.array([16, 1, 2, 0, 4, 2, 7, 1, 2, 14])
+
     print(fuel_consumption(data))  # Part 1 Solution: 342730
+    print(fuel_consumption(data, cost_scale="linear"))  # Part 2 Solution: 92335207
+    print(linear_scale_cost_function(data, np.mean(data)))  # 2335207
+    # Notice that the mean works for my given input data, though some searching
+    # (above or below the float value) for integers that work may be required.
+    # See TEST section below.
+
+    # TEST
+    print(fuel_consumption(test_data))  # Test 1 Solution: 37 is correct
+    print(fuel_consumption(test_data, cost_scale="linear"))  # Test 2 Solution: 168
+    print(linear_scale_cost_function(test_data, np.mean(test_data)))  # 170
