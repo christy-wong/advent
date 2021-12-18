9e4e0e03545dcc2ff4d52365768b3d27be461599
Ravi Dayabhai <ravi.dayabhai@chime.com>
2021-12-17 21:41:57 -0800

    Finish RD's Day 9, Part 1

--- day9/day9_rd.py
+++ day9/day9_rd.py
@@ -1,4 +1,5 @@
 import numpy as np
+from skimage.morphology import local_minima
 
 
 def read_data(raw_data_file: str) -> np.array:
@@ -9,6 +10,11 @@ def read_data(raw_data_file: str) -> np.array:
     return np.array(data).astype(int)
 
 
+def find_sum_of_risk_levels(data: np.array) -> np.array:
+    is_minima = local_minima(data, connectivity=1)
+    return np.sum(data[is_minima] + 1)
+
+
 if __name__ == "__main__":
     data = read_data('./vents.txt')
-    print(data)
+    print(find_sum_of_risk_levels(data))  # Part 1 Solution: 496
