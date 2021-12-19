d725f8a31a04214511d3282082229b0cee4faced
Ravi Dayabhai <ravi.dayabhai@chime.com>
2021-12-19 14:52:06 -0800

    Implement 4-way recursive flood-fill algo

--- day9/day9_rd.py
+++ day9/day9_rd.py
@@ -1,7 +1,7 @@
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
-from skimage.morphology import flood, local_minima
+from skimage.morphology import local_minima
 
 
 def read_data(raw_data_file: str) -> np.array:
@@ -21,13 +21,33 @@ def find_sum_of_risk_levels(data: np.array) -> np.array:
     return np.sum(data[is_minima] + 1)
 
 
-def get_basin_size(data: np.array, i: int, j: int) -> int:
+def is_inside_basin(data: np.array, i: int, j: int, basin_points: List[Tuple]) -> bool:
     """
-    Finds the size of the basin containing low point at position `i`, `j` in
-    `data`
+    Returns boolean for whether `i`, `j` is unfilled yet if inside basin or
     """
-    tolerance = 9 - 1 - data[i, j]
-    return np.sum(flood(data, (i, j), connectivity=1, tolerance=tolerance))
+    try:
+        if (data[i, j] == 9) or ((i, j) in basin_points) or (i < 0) or (j < 0):
+            return False
+    except IndexError:
+        return False
+
+    return True
+
+
+def recursive_flood(data: np.array, i: int, j: int, basin_points: List[Tuple]) -> int:
+    """
+    Finds the indices comprising the basin containing low point at position `i`, `j` in
+    `data` recursively
+    """
+    if not is_inside_basin(data, i, j, basin_points):
+        return
+
+    basin_points.append((i, j))
+
+    recursive_flood(data, i + 1, j, basin_points)
+    recursive_flood(data, i - 1, j, basin_points)
+    recursive_flood(data, i, j + 1, basin_points)
+    recursive_flood(data, i, j - 1, basin_points)
 
 
 def get_three_largest_basins(data: np.array) -> List[int]:
@@ -37,7 +57,9 @@ def get_three_largest_basins(data: np.array) -> List[int]:
     largest_basin_sizes = [0, 0, 0]
     low_point_indices = zip(*np.where(local_minima(data, connectivity=1)))
     for i, j in low_point_indices:
-        basin_size = get_basin_size(data, i, j)
+        basin_points = []
+        recursive_flood(data, i, j, basin_points)
+        basin_size = len(basin_points)
         smalled_basin_size_saved = min(largest_basin_sizes)
         if basin_size > smalled_basin_size_saved:
             largest_basin_sizes.remove(smalled_basin_size_saved)
@@ -47,12 +69,12 @@ def get_three_largest_basins(data: np.array) -> List[int]:
 
 def find_product_of_three_largest_basins(data: np.array) -> int:
     """
-    Multiples the size of the three largest basins together
+    Multiplies the size of the three largest basins together
     """
     return np.product(get_three_largest_basins(data))
 
 
 if __name__ == "__main__":
-    data = read_data("./vents.txt")
+    data = read_data("./vents_rd.txt")
     print(find_sum_of_risk_levels(data))  # Part 1 Solution: 496
     print(find_product_of_three_largest_basins(data))  # Part 2 Solution: 902880
